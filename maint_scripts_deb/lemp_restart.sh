3f6dddb6a22008e142ace88bc4f47d7c39cc7251
VanGaugh <vangaugh@outlook.com>
2024-10-22 17:47:06 +0800

    Update lemp_restart.sh

--- maint_scripts_deb/lemp_restart.sh
+++ maint_scripts_deb/lemp_restart.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+# RESTARTS LEMP SERVER (NGINX / MARIADB / PHP-FPM
+
 # Execute Commands
-sudo /etc/init.d/nginx restart && sudo /etc/init.d/php8.2-fpm restart && sudo /etc/init.d/mariadb restart
+sudo /etc/init.d/nginx restart && 
+sudo /etc/init.d/php8.2-fpm restart &&
+sudo /etc/init.d/mariadb restart
 
 echo "---------------------------------------------------------------------------------"
 echo -e "\n\033[32m Server Restart Complete!\033[0m\n"
