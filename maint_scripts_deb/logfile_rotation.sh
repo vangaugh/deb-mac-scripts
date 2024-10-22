a7434f612b1b0426a9acada24094686e80b2b0b4
VanGaugh <vangaugh@outlook.com>
2024-10-22 17:56:17 +0800

    Update logfile_rotation.sh

--- maint_scripts_deb/logfile_rotation.sh
+++ maint_scripts_deb/logfile_rotation.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# CUSTOM VARIABLES
 LOG_FILE="/var/log/logfile.log"
 MAX_SIZE=10485760 # 10 MB
 CURRENT_SIZE=$(wc -c <"${LOG_FILE}")
 
+# EXECUTE SCRIPT
 if [ "${CURRENT_SIZE}" -gt "${MAX_SIZE}" ]; then
     TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
     mv "${LOG_FILE}" "${LOG_FILE}_${TIMESTAMP}"
     touch "${LOG_FILE}"
 fi
+
+echo "...Logfile rotation complete. Summary located at '${LOG_FILE}_${TIMESTAMP}'"
